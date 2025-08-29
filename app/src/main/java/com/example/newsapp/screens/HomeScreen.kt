0c7a41ae84d40a648b325a53ea21d93f9403e453
angx7 <angelmirocu@gmail.com>
2025-08-28 19:53:28 -0600

    cards noticias relevantes

--- app/src/main/java/com/example/newsapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsapp/screens/HomeScreen.kt
@@ -1,15 +1,23 @@
 package com.example.newsapp.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -20,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import com.example.newsapp.components.HeaderTabs
 import com.example.newsapp.icons.Add_home
 import com.example.newsapp.ui.theme.NewsAppTheme
+import com.example.newsapp.ui.theme.RecentCards
 import com.example.newsapp.ui.theme.background
 
 @Composable
@@ -62,6 +72,8 @@ fun HomeScreen(innerPadding : PaddingValues) {
             }
         )
 
+        // Filtros
+
         HeaderTabs(
             tabs = listOf("Noticias", "Eventos", "Clima"),
             onTabSelected = { idx ->
@@ -72,8 +84,73 @@ fun HomeScreen(innerPadding : PaddingValues) {
                 }
             }
         )
-        // Filtros
-        Row {  }
+
+        // Encabezado
+
+        val noticias = listOf<String>(
+            "El presidente de EE.UU. no muestra signos de arrepentimiento...",
+            "La economía global enfrenta desafíos sin precedentes...",
+            "Científicos descubren una nueva especie en la Amazonía...",
+            "La tecnología 5G revoluciona las comunicaciones móviles...",
+            "El cambio climático amenaza la biodiversidad mundial...",
+            "Avances en inteligencia artificial transforman industrias...",
+        )
+
+        Text(
+            text = "Ultimas noticias",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 20.dp, bottom = 20.dp)
+        )
+
+
+
+        LazyRow (
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            items(noticias) { idx ->
+                Card(
+                    modifier = Modifier
+                        .width(250.dp)
+                        .height(200.dp)
+                        .padding(end = 10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = RecentCards
+                    ),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp),
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        Text(
+                            text = idx,
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            modifier = Modifier
+                                .padding(bottom = 10.dp)
+                        )
+                        Text(
+                            text = "febrero 08 - 2024",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            modifier = Modifier
+                                .padding(bottom = 10.dp)
+                        )
+                    }
+                }
+
+
+            }
+        }
+
+
+
     }
 }
 
